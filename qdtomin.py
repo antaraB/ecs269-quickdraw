c5529fdca53cc1b98abd963f63d71da0fbad2305
preritauti <preritauti@gmail.com>
2018-12-09 12:30:47 -0800

    Updated data convert file

--- qdtomin.py
+++ qdtomin.py
@@ -17,8 +17,10 @@ for (dirpath, dirnames, filenames) in walk(mypath):
 
 x_train = []
 x_test = []
+x_test_1 = []
 y_train = []
 y_test = []
+y_test_1 = []
 xtotal = []
 ytotal = []
 slice_train = int(2000/len(txt_name_list))  ###Setting value to be 80000 for the final dataset
@@ -27,27 +29,75 @@ i = 0
 seed = np.random.randint(1, 10e6)
 
 
-##Creates test/train split with quickdraw data
+# ##Creates test/train split with quickdraw data
+# for txt_name in txt_name_list:
+#     txt_path = mypath + txt_name
+#     x = np.load(txt_path)
+#     x = x.astype('float32') / 255.    ##scale images
+#     y = [i] * len(x)  
+#     # np.random.seed(seed)
+#     # np.random.shuffle(x)
+#     # np.random.seed(seed)
+#     # np.random.shuffle(y)
+#     x = x[:slice_train]
+#     y = y[:slice_train]
+#     if i != 0: 
+#         xtotal = np.concatenate((x,xtotal), axis=0)
+#         ytotal = np.concatenate((y,ytotal), axis=0)
+#     else:
+#         xtotal = x
+#         ytotal = y
+#     i += 1
+# x_train, x_test, y_train, y_test = train_test_split(xtotal, ytotal, test_size=0.2, shuffle=False)
+
+
 for txt_name in txt_name_list:
     txt_path = mypath + txt_name
+    print("txt_name : ",txt_name)
+    print("label : ",i)
     x = np.load(txt_path)
     x = x.astype('float32') / 255.    ##scale images
     y = [i] * len(x)  
-    np.random.seed(seed)
-    np.random.shuffle(x)
-    np.random.seed(seed)
-    np.random.shuffle(y)
-    x = x[:slice_train]
-    y = y[:slice_train]
+    # np.random.seed(seed)
+    # np.random.shuffle(x)
+    # np.random.seed(seed)
+    # np.random.shuffle(y)
+    x = x[:1100]
+    y = y[:1100]
+    xt = x[-100:]
+    yt = y[-100:]
+    x = x[:1000]
+    y = y[:1000]
     if i != 0: 
-        xtotal = np.concatenate((x,xtotal), axis=0)
-        ytotal = np.concatenate((y,ytotal), axis=0)
+        x_train = np.concatenate((x,x_train), axis=0)
+        y_train = np.concatenate((y,y_train), axis=0)
+        x_test = np.concatenate((xt,x_test), axis=0)
+        y_test = np.concatenate((yt,y_test), axis=0)
     else:
-        xtotal = x
-        ytotal = y
+        x_train = x
+        y_train = y
+        x_test = xt
+        y_test = yt
     i += 1
-x_train, x_test, y_train, y_test = train_test_split(xtotal, ytotal, test_size=0.2, random_state=42)
+    # np.random.seed(seed)
+    # np.random.shuffle(x_train)
+    # np.random.seed(seed)
+    # np.random.shuffle(y_train)
+print("x_train.shape",x_train.shape)
+print("y_train.shape",y_train.shape)
+print("x_test.shape",x_test.shape)
+print("y_test.shape",y_test.shape)
 
+
+x_train, x_test_1, y_train, y_test_1 = train_test_split(x_train, y_train, test_size=0, random_state=42)
+
+
+print("x_test_1.shape",x_test_1.shape)
+print("y_test_1.shape",y_test_1.shape)
+print("x_train.shape",x_train.shape)
+print("y_train.shape",y_train.shape)
+print("x_test.shape",x_test.shape)
+print("y_test.shape",y_test.shape)
 
 
 ##Saves this out as hdf5 format
@@ -64,9 +114,19 @@ data_to_write = y_train
 with h5py.File('y_train.h5', 'w') as hf:
     hf.create_dataset("name-of-dataset",  data=data_to_write)
 
+print("y train first 10")
+print(y_train[:20])
+
+print("y test first 10")
+print(y_test[:10])
+
+print("x_train")
+print(x_train)
 
 ##Visualize a quickdraw file
 import matplotlib.pyplot as plt
-face1 = x_train[5].reshape(28,28)
+face1 = x_test[0].reshape(28,28)
+print(y_test[0])
 plt.imshow(face1)
 plt.show()
+
